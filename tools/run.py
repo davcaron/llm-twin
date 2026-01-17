1a8e5cb629080fc5a42de5365bdbb333dac93572
davcaron <90650484+davcaron@users.noreply.github.com>
2026-01-17 18:39:11 -0500

    added feature engineering pipeline skeleton + query_data_warehouse func to retrieve data from MongoDB DW

---
+++ tools/run.py
@@ -0,0 +1,80 @@
+from datetime import datetime as dt
+from pathlib import Path
+
+import click
+from loguru import logger
+
+from llm_twin import settings
+from pipelines.feature_engineering import feature_engineering
+
+
+@click.command(
+    help="""
+LLM Engineering project CLI v0.0.1. 
+
+Main entry point for the pipeline execution. 
+This entrypoint is where everything comes together.
+
+Run the ZenML LLM Engineering project pipelines with various options.
+
+Run a pipeline with the required parameters. This executes
+all steps in the pipeline in the correct order using the orchestrator
+stack component that is configured in your active ZenML stack.
+
+Examples:
+
+  \b
+  # Run the pipeline with default options
+  python run.py
+               
+  \b
+  # Run the pipeline without cache
+  python run.py --no-cache
+  
+  \b
+  # Run only the ETL pipeline
+  python run.py --only-etl
+
+"""
+)
+@click.option(
+    "--no-cache",
+    is_flag=True,
+    default=False,
+    help="Disable caching for the pipeline run.",
+)
+@click.option(
+    "--run-feature-engineering",
+    is_flag=True,
+    default=False,
+    help="Whether to run the FE pipeline.",
+)
+def main(
+    no_cache: bool = False,
+    run_feature_engineering: bool = False,
+    export_settings: bool = False,
+) -> None:
+    assert (
+        run_feature_engineering or export_settings
+    ), "Please specify an action to run."
+
+    if export_settings:
+        logger.info("Exporting settings to ZenML secrets.")
+        settings.export()
+
+    pipeline_args = {
+        "enable_cache": not no_cache,
+    }
+    root_dir = Path(__file__).resolve().parent.parent
+
+    if run_feature_engineering:
+        run_args_fe = {}
+        pipeline_args["config_path"] = root_dir / "configs" / "feature_engineering.yaml"
+        pipeline_args["run_name"] = (
+            f"feature_engineering_run_{dt.now().strftime('%Y_%m_%d_%H_%M_%S')}"
+        )
+        feature_engineering.with_options(**pipeline_args)(**run_args_fe)
+
+
+if __name__ == "__main__":
+    main()
